cabb5f088f4ca8de3ddb395e3fc9005cb509b6f8
Prathika <lkprathika@gmail.com>
2026-08-13 21:24:44 +0530

    proximity detection for suspicious device flagging

--- core/tracker.py
+++ core/tracker.py
@@ -5,23 +5,32 @@ class PersistenceEngine:
         self.window_seconds = window_seconds
         self.observations = {}
 
-    def add_observation(self, mac, timestamp=None):
+    def add_observation(self, mac, distance, timestamp=None):
         if timestamp is None:
             timestamp = time.time()
             
         if mac not in self.observations:
             self.observations[mac] = []
             
-        self.observations[mac].append(timestamp)
+        self.observations[mac].append((timestamp, distance))
         self._cleanup(mac)
 
     def get_observations(self, mac):
         self._cleanup(mac)
         return self.observations.get(mac, [])
 
+    def is_suspicious(self, mac, dist_threshold=3.0, min_observations=5):
+        obs = self.get_observations(mac)
+        if len(obs) < min_observations:
+            return False
+            
+        close_sightings = [d for t, d in obs if d <= dist_threshold]
+        return len(close_sightings) >= min_observations
+
     def _cleanup(self, mac):
         now = time.time()
-        self.observations[mac] = [
-            t for t in self.observations[mac] 
-            if now - t <= self.window_seconds
-        ]
+        if mac in self.observations:
+            self.observations[mac] = [
+                item for item in self.observations[mac] 
+                if now - item[0] <= self.window_seconds
+            ]
