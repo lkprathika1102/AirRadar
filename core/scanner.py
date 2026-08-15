f25f707bdc4ddb23c8c7a12172df4e9a9fe5c7ab
Prathika <lkprathika@gmail.com>
2026-08-15 20:46:27 +0530

    integrate scanner with signal processor

--- core/scanner.py
+++ core/scanner.py
@@ -1,18 +1,38 @@
 import asyncio
 from bleak import BleakScanner
+from core.filter import SignalProcessor
+from core.tracker import PersistenceEngine
 
 class BLEManager:
     def __init__(self, detection_callback):
         self.detection_callback = detection_callback
+        self.processor = SignalProcessor()
+        self.tracker = PersistenceEngine()
         self.scanner = BleakScanner()
 
+    async def handle_detection(self, device, advertisement_data):
+        mac = device.address
+        rssi = advertisement_data.rssi
+        
+        filtered_rssi, distance = self.processor.process(mac, rssi)
+        self.tracker.add_observation(mac, distance)
+        is_threat = self.tracker.is_suspicious(mac)
+        
+        payload = {
+            "mac": mac,
+            "name": device.name,
+            "rssi": rssi,
+            "filtered_rssi": filtered_rssi,
+            "distance": distance,
+            "is_threat": is_threat
+        }
+        
+        await self.detection_callback(payload)
+
     async def start_scan(self):
-        await self.scanner.start()
+        await self.scanner.start(detection_callback=self.handle_detection)
         try:
             while True:
-                devices = await self.scanner.get_discovered_devices()
-                for device in devices:
-                    await self.detection_callback(device)
                 await asyncio.sleep(1.0)
         finally:
             await self.scanner.stop()
